bccdf362cb627b111e2233aa6cb7e52e76bce195
viniciussti <viniciussantos8@hotmail.com>
2021-07-22 22:06:49 -0300

    Implementando todo conteúdo do site através do bootstrap

---
+++ script.js
@@ -0,0 +1,42 @@
+let nome = document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
+
+
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido!'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'E-mail válido!'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+
+    if (assunto.value.length >= 300) {
+        txtAssunto.innerHTML = 'Limite de 300 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha o formulário corretamente!')
+    }
+}
